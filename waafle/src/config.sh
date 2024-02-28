5694ad0e58113651466965fff06e904d14366af8
giacomoorsini <133371083+giacomoorsini@users.noreply.github.com>
2024-02-28 12:57:30 +0100

    Create config.sh

---
+++ waafle/src/config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#MAIN PATH
+MAINDIR="/users/genomics/gorsini"
+
+#DATA PATH
+DATADIR=$MAINDIR"/giacomoAssembled/assembled_C16-20082-TZ"  
+DBDIR=$MAINDIR"/tools/HGT/waafle/waafledb/waafledb"
+TAXONOMY=$MAINDIR"/tools/HGT/waafle/waafledb_taxonomy.tsv"
+
+#INPUT PATH
+INPUTFILE=$DATADIR"/final.contigs.fa"
+
+#TOOL PATH
+TOOL=$MAINDIR"/tools/HGT/waafle"
+
+#SCRIPT AND OUPUT PATH
+SRCDIR=$TOOL"/src"
+STDDIR=$TOOL"/stdir"
+
+#STEP PATH
+SEARCH=$STDDIR"/search"
+GENECALL=$STDDIR"/genecall"
+ORGOSCORE=$STDDIR"/orgscore"
+
+#ERROR AND OUTPUT PATH
+SEARCHOUT=$SEARCH"/out"
+SEARCHERR=$SEARCH"/err"
+SEARCHFINAL=$SEARCH"/final_out"
+
+GCALLOUT=$GENECALL"/out"
+GCALLERR=$GENECALL"/err"
+GCALLFINAL=$GENECALL"/final_out"
+
+OSCOROUT=$ORGOSCORE"/out"
+OSCORERR=$ORGOSCORE"/err"
+OSCORFINAL=$ORGOSCORE"/final_out"
