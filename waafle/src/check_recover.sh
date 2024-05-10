938f76144f5ad97dea9e274d2887671e34676ae7
Giacomo Orsini <133371083+giacomoorsini@users.noreply.github.com>
2024-05-10 15:00:17 +0200

    Create check_recover.sh

---
+++ waafle/src/check_recover.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#PBS -N recover
+#PBS -l select=1:ncpus=1:mem=5gb
+#PBS -l walltime=06:00:00
+#PBS -q CIBIO_cpuQ
+#PBS -o "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghanatanzania_animals/stdir/recover.out"
+#PBS -e "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghanatanzania_animals/stdir/recover.err"
+
+# Import paths from config file
+source /home/giacomo.orsini-1/waafle/test_anal/src/config.sh
+
+# number of files in the temporary directory (if the file is still there it means that the script didn't run succesfully)
+num_files=$(ls $TMP -1 | wc -l)
+
+# Checkpoint
+if [ "$wc" -eq 0]; then
+        echo "There are no files in data directory, all the analysis ran correctly. Finished."
+        exit 0
+fi
+
+echo "In data directory there are $num_files contigs file that have not been analyzed correctly, calling waafle_run again..."
+
+# Go trough the files in the directory and send again the waafle job
+for file in $TMP/*; do
+
+        # Extract filename
+        filename=$(basename $file)
+        JOBID=$(echo "$filename" | awk -F "_" '{print $1}' | awk -F "-" '{print $2"-"$3}' )
+
+        # Checkpoint
+        if [ $? -eq 0 ]; then
+                echo "Analyzing sample: $JOBID"
+        else
+                echo "Failed to retrieve the sample name from folder. Check $TMP. Aborting"
+                exit 1
+        fi
+        
+        # Send job to cluster
+        qsub -N $JOBID -o $STDDIR/waafle_${JOBID}.out -e $STDDIR/waafle_${JOBID}.err -q $QUEUE $SRCDIR/waafle_run.sh
+
+        # Checkpoint
+        if [ $? -eq 1 ]; then
+                echo -e "\t Unable to send job to the cluster. Aborting."
+                exit 1
+        else
+                echo -e "\t Job sent."
+        fi
+
+        sleep 2
+
+done
+
+# Checkpoint
+if [ $? -eq 0 ]; then
+        echo "Gone trough all the files in the directory. Finished."
+else
+        echo "Failed to go trough the files in the directoryry. Aborting."
+        exit 1
+fi
