943f557f30d75db43869da4ee363946a5fc69028
Giacomo Orsini <133371083+giacomoorsini@users.noreply.github.com>
2024-10-29 18:34:25 +0100

    Update check_recover.sh

--- waafle/src/PBS_final_scripts/check_recover.sh
+++ waafle/src/PBS_final_scripts/check_recover.sh
@@ -4,11 +4,11 @@
 #PBS -l select=1:ncpus=1:mem=5gb
 #PBS -l walltime=06:00:00
 #PBS -q CIBIO_cpuQ
-#PBS -o "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghanatanzania_animals/stdir/recover.out"
-#PBS -e "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghanatanzania_animals/stdir/recover.err"
+#PBS -o "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghana/stdir/check_recover.out"
+#PBS -e "/shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghana/stdir/check_recover.err"
 
 # Import paths from config file
-source /home/giacomo.orsini-1/waafle/test_anal/src/config.sh
+source /shares/CIBIO-Storage/CM/scratch/users/giacomo.orsini/waafle/CM_ghana/src/config.sh
 
 # number of files in the temporary directory (if the file is still there it means that the script didn't run succesfully)
 num_files=$(ls $TMP -1 | wc -l)
@@ -21,12 +21,24 @@ fi
 
 echo "In data directory there are $num_files contigs file that have not been analyzed correctly, calling waafle_run again..."
 
+ls $TMP
+
 # Go trough the files in the directory and send again the waafle job
 for file in $TMP/*; do
 
+        if [[ $file == *.bz2 ]]; then
+                bzip2 -d $file
+                if [ $? -eq 0 ]; then
+                        echo "Unable to unzip the file"
+                else
+                        echo "unzipping the file"
+                fi
+        fi
+
         # Extract filename
         filename=$(basename $file)
-        JOBID=$(echo "$filename" | awk -F "_" '{print $1}' | awk -F "-" '{print $2"-"$3}' )
+
+        JOBID=$(echo "$filename" | awk -F "$SUFFIX" '{print $1}' | awk -F "-" '{print $2"-"$3}' )
 
         # Checkpoint
         if [ $? -eq 0 ]; then
@@ -35,7 +47,7 @@ for file in $TMP/*; do
                 echo "Failed to retrieve the sample name from folder. Check $TMP. Aborting"
                 exit 1
         fi
-        
+
         # Send job to cluster
         qsub -N $JOBID -o $STDDIR/waafle_${JOBID}.out -e $STDDIR/waafle_${JOBID}.err -q $QUEUE $SRCDIR/waafle_run.sh
 
@@ -54,6 +66,10 @@ done
 # Checkpoint
 if [ $? -eq 0 ]; then
         echo "Gone trough all the files in the directory. Finished."
+else
+        echo "Failed to go trough the files in the directoryry. Aborting."
+        exit 1
+fi
 else
         echo "Failed to go trough the files in the directoryry. Aborting."
         exit 1
