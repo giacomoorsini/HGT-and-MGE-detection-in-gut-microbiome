966bf0e13c7b4949418cc8bd0efab71dac1ef82f
Giacomo Orsini <133371083+giacomoorsini@users.noreply.github.com>
2024-10-30 12:09:57 +0100

    Delete waafle/src/orgscorer_v1.sh

--- waafle/src/orgscorer_v1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source config.sh 
-
-cat $GCALLFINAL/* > $GCALLFINAL"/genecall.gff"
-cat $SEARCHFINAL/* > $SEARCHFINAL"/search.blastout"
-
-sbatch --output=$OSCOROUT"/orgscorer.out" --error=$OSCORERR"/orgscorer.err" waafle_orgscorer.sh
